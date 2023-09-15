59e1d3d30d160f59190f4b836c9d79a3398a8f5c
leviethung220703@gmail.com <leviethung220703@gmail.com>
2023-09-15 15:34:19 +0700

    create model

---
+++ Api_ZooManagement_SWP391/Api_ZooManagement_SWP391/Entities/Order.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
+
+namespace Api_ZooManagement_SWP391.Entities
+{
+    public class Order
+    {
+        [Required]
+        [StringLength(10)]
+        public string OrderId { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email {  get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string FullName { get; set; }
+        [Required]
+        [StringLength(10)]
+        public string Phone { get; set; }
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime BuyDate { get; set; }
+
+        [Range(0,50)]
+        public double TotalPrice {  get; set; }
+
+        public ICollection<OrderDetail> OrderDetails { get; set; }
+
+        public Guest Guest { get; set; }
+    }
+}
