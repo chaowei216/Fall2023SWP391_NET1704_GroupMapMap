2d5d2a41f72c045421722e7d5bb98cd9800fa0d7
YukiYuto <bestzed12345ff@gmail.com>
2023-09-16 09:02:16 +0700

    Animal, AnimalCages, Feedschedule, Food, SpeciesAnimal

---
+++ Api_ZooManagement_SWP391/Api_ZooManagement_SWP391/Entities/Animal.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api_ZooManagement_SWP391.Entities
+{
+    public class Animal
+    {
+        [Required]
+        [MaxLength(6)]
+        public string AnimalID { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Name {  get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public bool Sex { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Type {  get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateOnly EntryDate { get; set; }
+
+        [Required]
+        public string Region { get; set; }
+
+        [Required]
+        public string HealthCheck { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateOnly Birthday { get; set; }
+
+        public SpeciesAnimal SpeciesAnimal { get; set; }
+
+        public ICollection<AnimalCages> AnimalCages { get; set; }
+        public ICollection<Feedschedule> Feedschedules { get; set; }
+        public ICollection<AnimalTrainer> AnimalTrainers { get; set; }
+    }
+}
---
+++ Api_ZooManagement_SWP391/Api_ZooManagement_SWP391/Entities/SpeciesAnimal.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api_ZooManagement_SWP391.Entities
+{
+    public class SpeciesAnimal
+    {
+        [Required]
+        [MaxLength(5)]
+        [RegularExpression(@"^A\d{5}$")]
+        public string SpeciesId { get; set; }
+
+        [Required]
+        public string SpeciesName { get; set; }
+
+    }
+}
