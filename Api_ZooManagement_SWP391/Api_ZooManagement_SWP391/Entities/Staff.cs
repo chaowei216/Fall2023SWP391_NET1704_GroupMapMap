ffce5d6a5e1e35a6a9a58ea1c8990ca9b62a0610
Hung Coder <97719955+hungvietle227@users.noreply.github.com>
2023-09-17 14:45:19 +0700

    Revert "Dev branch"

--- Api_ZooManagement_SWP391/Api_ZooManagement_SWP391/Entities/Staff.cs
+++ Api_ZooManagement_SWP391/Api_ZooManagement_SWP391/Entities/Staff.cs
@@ -1,30 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Api_ZooManagement_SWP391.Entities
+﻿namespace Api_ZooManagement_SWP391.Entities
 {
     public class Staff
     {
-        [Required]
-        [StringLength(10)]
-        public string SId { get; set; }
-        [Required]
-        [StringLength(30)]
+        
+        public int Id { get; set; }
         public string FullName { get; set; }
-        [EmailAddress]
         public string Email { get; set; }
-        [Phone]
         public string Phone { get; set; }
-        [StringLength(30)]
         public string Address { get; set; }
-        [Required]
-        public bool Sex { get; set; }
+        public bool sex { get; set; }
 
-        public Account Account { get; set; }
-
-        public ICollection<ZooTrainer> ZooTrainers { get; set; }
-
-        public Area Area { get; set; }
-
-
-}
+        
+    }
 }
