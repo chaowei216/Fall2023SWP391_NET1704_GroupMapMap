8a2623b934d30af9e5470daa298ce9e736308120
YukiYuto <bestzed12345ff@gmail.com>
2023-10-03 12:20:29 +0700

    add food

---
+++ Api_ZooManagement_SWP391_Server/Api_ZooManagement_SWP391/Dtos/FoodDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Api_ZooManagement_SWP391.Dtos
+{
+    public class FoodDto
+    {
+        public string FName {  get; set; }
+        public int Quantity { get; set; }
+        public DateTime ImportDate { get; set; }
+        public DateTime ExpiredDate {  get; set; }
+        public string Category {  get; set; }
+    }
+}
